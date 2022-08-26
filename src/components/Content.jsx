d4e1efa4a3cf45eecfd01b5f7b134218c5618c3c
[Github <[Github>
2022-08-26 23:55:21 +0900

    feat : 컨텐츠 추가 - 사극 장르 삭제.
    design : 발매 날짜가 9이하라면 숫자 앞에 0 추가.
    
    design : 썸네일 이미지 꽉차게 수정.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
+
+
 export default function Content(props) {
+
+    // 만약 개봉 날짜가 10 미만이면, 0을 붙이기.
+    let release_date_zero = "";
+    if (props.release[1]<10){
+        release_date_zero = "0"
+    }
+
     return (
     <li className='content' key={props.id}>
         <Link to={props.link} >
             <div className='content_img' > <img src={"/server/img/"+props.img} alt="" /> </div>
             <h4 className='content_title'>{props.title}</h4>
-            <p className='content_release'>{props.release[0]+". "+props.release[1]+"."}</p>
+            <p className='content_release'>{props.release[0]+". "+release_date_zero+props.release[1]+"."}</p>
         </Link>
     </li>
     )
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -33,21 +33,11 @@ for (let j = 0; j < props.content.keyword.length; j++) {
     )
 }
 
-// function CopyUrlToClipboard()
-// {
-//     var url = '';    // <a>태그에서 호출한 함수인 clip 생성
-//     var textarea = document.createElement("textarea");  
-//     //url 변수 생성 후, textarea라는 변수에 textarea의 요소를 생성
-    
-//     document.body.appendChild(textarea); //</body> 바로 위에 textarea를 추가(임시 공간이라 위치는 상관 없음)
-//     url = window.document.location.href;  //url에는 현재 주소값을 넣어줌
-//     textarea.value = url;  // textarea 값에 url를 넣어줌
-//     textarea.select();  //textarea를 설정
-//     console.log(textarea)
-//     navigator.clipboard.writeText(textarea)
-//     document.body.removeChild(textarea); //extarea 요소를 없애줌
-//     alert("URL이 복사되었습니다.")  // 알림창
-// }
+// 만약 개봉 날짜가 10 미만이면, 0을 붙이기.
+let release_date_zero = "";
+if (props.content.release[1]<10){
+    release_date_zero = "0"
+}
 
 return (
     <section className='detail_wrap'>
@@ -79,7 +69,7 @@ return (
                 </div>
 
                 <div className='detail_title_box'>
-                    <p className='detail_title_mini'>{props.content.englishTitle+" │ "+props.content.release[0]+". "+props.content.release[1]+"."+" │ "+props.content.country}</p>
+                    <p className='detail_title_mini'>{props.content.englishTitle+" │ "+props.content.release[0]+". "+release_date_zero+props.content.release[1]+"."+" │ "+props.content.country}</p>
                     <h2 className='detail_title'>{text_space(props.content.title)}</h2> 
                 </div>
 
